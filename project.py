081f7b5efdc29f85124d303d185a750c358c03cc
Kashish <write2amreen@gmail.com>
2021-11-20 16:06:25 +0400

    Converted lists to strings
    user enters in lists, the program coverts lists to string

--- project.py
+++ project.py
@@ -7,12 +7,26 @@ Created on Sat Nov 13 19:56:17 2021
 
 #function definition that will accept
 #a string paameter, count the no. of times a character is present inside it
-def countchar(str,ch):
-    count=0
-    for i in str:
-        if i==ch:
-            count+=1
+
+#Function that returns frequency of a character
+def countchar(list, ch):
+    count = 0
+    for char in list:
+        for i in char:
+            if i == ch:
+                count = count+1
     return count
+
+
+def createlist(list):
+    number_of_elements = int(
+        input("Enter the number of elements you want in the list: ")) #Asks the user to enter number of elements
+    for i in range(number_of_elements):
+        str_input = str(input("Enter the element: ")) #Asks the user to enter the elements
+        list.append(str_input)
+    print(list) #Prints the list
+    print("N O W    C O N V E R T I N G    T H E    A B O V E    L I S T   T O    S T R I N G")    
+
 
 #main code
 #function calls
